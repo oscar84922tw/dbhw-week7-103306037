35708a34d87bfec4ee9d44ad0cf089091b0e230b
Oscar <chenhuawei@chenhuaweis-MacBook-Pro.local>
2016-05-17 12:41:31 +0800

    init

--- app.js
+++ app.js
@@ -4,15 +4,17 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var articles = require('./routes/articles.js');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -20,10 +22,13 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({secret : 'HelloExpressSESSION'}));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/articles', articles);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,20 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: '首頁',
+    who: req.session.who
+  });
+});
+
+router.post('/verify', function(req, res) {
+	if (req.body.username) {
+		req.session.who = req.body.username;
+	}
+	res.render('index', {
+		title : '首頁',
+		who : req.session.who
+	});
 });
 
 module.exports = router;
